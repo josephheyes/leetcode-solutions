6a90ea3cf7db2350c56fcd94824a444f3f328cd0
Joseph Heyes <psyjh23@nottingham.ac.uk>
2022-09-18 13:49:53 +0100

    fix project layout

--- solutions/1470-Shuffle-The-Array.java
+++ solutions/1470-Shuffle-The-Array.java
@@ -1,4 +1,11 @@
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] nums = {2,5,1,3,4,7};
+        System.out.println(Arrays.toString(shuffle(nums, 3)));
+    }
+
     public static int[] shuffle(int[] nums, int n) {
         int[] result = new int[nums.length];
 
