74c31fd525a6ab44068ac3e3cba3e82bb712ac02
Joseph Heyes <josephheyes@gmail.com>
2022-12-25 23:42:35 +0000

    add python two sum solution

---
+++ solutions/python/1-Two-Sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        hashmap = {}
+        for i, num in enumerate(nums):
+            if target-num in hashmap:
+                return [i, hashmap[target-num]]
+            hashmap[num] = i
